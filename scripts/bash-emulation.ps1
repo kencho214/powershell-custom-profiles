a42739f093ea0571f91df76092243e723e179712
Kento Ueda <ueda.kento@mail.glory.co.jp>
2019-05-31 19:29:21 +0900

    [add] some commands
    where
    pwd
    touch
    vim
    vi
    ln
    mklink
    unlink

--- scripts/bash-emulation.ps1
+++ scripts/bash-emulation.ps1
@@ -1,7 +1,71 @@
-# Alias
-Set-Alias grep Select-String
+# grep
+function grep { Select-String $args }
 
-# Function
+# printenv
 function printenv { Get-ChildItem Env: }
+
+# which
 function which { (Get-Command $args).Definition }
 
+# where
+Remove-Item alias:where -Force
+function where { (Get-Item (Get-Command $args).Path).Directory.FullName }
+
+# pwd
+Remove-Item alias:pwd -Force
+function pwd { $(Get-Location).Path }
+
+# touch
+function touch { Write-Host "" > $args }
+
+# vim
+function vim {
+  $vim = (Join-Path (Get-Item (Get-Command git).Path).Directory.Parent.FullName "usr\bin\vim.exe")
+  .$vim $args
+}
+
+# vi
+function vi { vim $args }
+
+# ln
+function ln {
+  if (($args[0] -eq "-h") -or ($args[0] -eq "--help")) {
+    Write-Host "create symbolic link"
+    Write-Host "ln -s [link name] [target path]"
+    return
+  }
+  if (!($args[0] -eq "-s")) {
+    Write-Host "command is missing (-s)"
+    Write-Host "ln -s [link name] [target path]"
+    return
+  }
+  if ($args.Length -lt 3) {
+    Write-Host "missing params."
+    Write-Host "ln -s [link name] [target path]"
+    return
+  }
+
+  New-Item -ItemType SymbolicLink -Name $args[1] -Target $args[2]
+}
+
+# mklink
+function mklink { ln $args }
+
+# unlink
+function unlink {
+  Param($path)
+
+  if (!(Test-Path $path)) {
+    Write-Host "not found"
+    return;
+  }
+
+  $file = Get-Item $path -Force -ea SilentlyContinue
+  if (!($file.Attributes -band [IO.FileAttributes]::ReparsePoint)) {
+    $file.Is
+    Write-Host "file is not symlink"
+    return;
+  }
+
+  $file.Delete()
+}
