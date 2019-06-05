26eb6623a9cdc5143fa4afeca95182d5219f42e0
Kento Ueda <ueda.kento@mail.glory.co.jp>
2019-06-05 14:28:55 +0900

    [add] env control functions

---
+++ scripts/env-control.ps1
@@ -0,0 +1,78 @@
+function userenv {
+  if ($args.Length -eq 0) {
+    printenv
+    return
+  }
+
+  ($key, $value) = $args[0] -split "="
+
+  if (!$value) {
+    [System.Environment]::GetEnvironmentVariable($args[0])
+    return
+  }
+
+  if ($key -eq "path") {
+    Write-Host "path should not overwrite by this command." -ForegroundColor Red
+    Write-Host "use addpath/rmpath/getpath instead." -ForegroundColor Yellow
+    return
+  }
+
+  set-item "env:${key}" $value
+  [System.Environment]::SetEnvironmentVariable($key, $value, "User")
+}
+
+function getpath {
+  $paths = $([System.Environment]::GetEnvironmentVariable("path", "User")) -split ";"
+
+  foreach ($arg in $paths) {
+    Write-Host $arg
+  }
+}
+
+function addpath {
+  $paths = $([System.Environment]::GetEnvironmentVariable("path", "User")) -split ";"
+
+  if ($args.Length -eq 0) {
+    Write-Error "addpath [path1] [path2] ..."
+    return
+  }
+
+  $list = New-Object System.Collections.ArrayList
+
+  foreach ($elm in ($paths + $args)) {
+    if (!$elm) { continue }
+    if ($list -contains $elm) { continue }
+    $list += $elm
+  }
+
+  foreach ($elm in $list) { Write-Host $elm }
+
+  $tmpPath = $list -join ";"
+
+  set-item "env:path" $tmpPath
+  [System.Environment]::SetEnvironmentVariable("path", $tmpPath, "User")
+}
+
+function rmpath {
+  $paths = $([System.Environment]::GetEnvironmentVariable("path", "User")) -split ";"
+
+  if ($args.Length -eq 0) {
+    Write-Error "rmpath [path1] [path2] ..."
+    return
+  }
+
+  $list = New-Object System.Collections.ArrayList
+
+  foreach ($elm in $paths) {
+    if (!$elm) { continue }
+    if ($args -contains $elm) { continue }
+    $list += $elm
+  }
+
+  foreach ($elm in $list) { Write-Host $elm }
+
+  $tmpPath = $list -join ";"
+
+  set-item "env:path" $tmpPath
+  [System.Environment]::SetEnvironmentVariable("path", $tmpPath, "User")
+}
