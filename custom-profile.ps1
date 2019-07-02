fdf7ee923062b56999964631be32b3fe9b2fdb6e
Kento Ueda <kencho214@gmail.com>
2019-07-03 00:35:09 +0900

    [add] add prompt override

--- custom-profile.ps1
+++ custom-profile.ps1
@@ -2,4 +2,13 @@
 # PSCustomScripts must be defined as a environment variable
 Get-ChildItem $Env:PSCustomScripts\scripts | Where-Object Extension -eq ".ps1" | ForEach-Object{.$_.FullName}
 
+function prompt {
+  $origLastExitCode = $LASTEXITCODE
+  $prompt = Write-Host ($ExecutionContext.SessionState.Path.CurrentLocation) -ForegroundColor Green -nonewline
+  $prompt += Write-VcsStatus
+  $LASTEXITCODE = $origLastExitCode
+  $prompt += "$(' $' * ($nestedPromptLevel + 1))"
+  if ($prompt) { "$prompt " } else { " " }
+}
+
 Write-Host "Custom PowerShell Environment Loaded" -ForegroundColor DarkGray
