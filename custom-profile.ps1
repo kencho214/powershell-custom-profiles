678de31350143c3eeae3e9c85321a96fa43203d5
Kento Ueda <ueda.kento@mail.glory.co.jp>
2019-07-04 13:15:56 +0900

    [modify] head of script line to abbreviate path

--- custom-profile.ps1
+++ custom-profile.ps1
@@ -4,11 +4,23 @@ Get-ChildItem $Env:PSCustomScripts\scripts | Where-Object Extension -eq ".ps1" |
 
 function prompt {
   $origLastExitCode = $LASTEXITCODE
-  $prompt = Write-Host ($ExecutionContext.SessionState.Path.CurrentLocation) -ForegroundColor Green -nonewline
-  $prompt += Write-VcsStatus
+  $dirs = $ExecutionContext.SessionState.Path.CurrentLocation -split "\\"
+  $pwd = $dirs[0]
+  if ($dirs.Length -gt 2) {
+    $intermediates += $dirs[1..($dirs.Length - 2)] | ForEach-Object { $_.Substring(0, 1) }
+    $pwd += "\"
+    $pwd += $intermediates -join "\"
+  }
+  if ($dirs.Length -gt 1) {
+    $pwd += "\" + $dirs[-1]
+  }  
+   
+  # Write-Host ($ExecutionContext.SessionState.Path.CurrentLocation) -ForegroundColor Green -nonewline
+  Write-Host ($pwd) -ForegroundColor Green -nonewline
+  Write-VcsStatus
+  # Write-Host
   $LASTEXITCODE = $origLastExitCode
-  $prompt += "$(' $' * ($nestedPromptLevel + 1))"
-  if ($prompt) { "$prompt " } else { " " }
+  "$(' $' * ($nestedPromptLevel + 1)) "
 }
 
 Write-Host "Custom PowerShell Environment Loaded" -ForegroundColor DarkGray
