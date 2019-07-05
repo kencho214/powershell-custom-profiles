50271c2bedaad47ceb309a7cd9628d1bcd9cf524
Kento Ueda <ueda.kento@mail.glory.co.jp>
2019-07-05 12:37:00 +0900

    [modify] prompt to abbreviate home dir

--- custom-profile.ps1
+++ custom-profile.ps1
@@ -1,22 +1,29 @@
 # set dir name to load custom scripts automatically
 # PSCustomScripts must be defined as a environment variable
-Get-ChildItem $Env:PSCustomScripts\scripts | Where-Object Extension -eq ".ps1" | ForEach-Object{.$_.FullName}
+Get-ChildItem $Env:PSCustomScripts\scripts | Where-Object Extension -eq ".ps1" | ForEach-Object { .$_.FullName }
 
 function prompt {
   $origLastExitCode = $LASTEXITCODE
-  $dirs = $ExecutionContext.SessionState.Path.CurrentLocation -split "\\"
-  $pwd = $dirs[0]
+
+  $currentDir = $ExecutionContext.SessionState.Path.CurrentLocation.ToString()
+
+  if ($currentDir.contains($home)) {
+    $currentDir = $currentDir.Replace($home, "~")
+  }
+
+  $dirs = $currentDir -split "\\"
+  $currentDir = $dirs[0]
   if ($dirs.Length -gt 2) {
     $intermediates += $dirs[1..($dirs.Length - 2)] | ForEach-Object { $_.Substring(0, 1) }
-    $pwd += "\"
-    $pwd += $intermediates -join "\"
+    $currentDir += "\"
+    $currentDir += $intermediates -join "\"
   }
   if ($dirs.Length -gt 1) {
-    $pwd += "\" + $dirs[-1]
-  }  
-   
+    $currentDir += "\" + $dirs[-1]
+  }
+
   # Write-Host ($ExecutionContext.SessionState.Path.CurrentLocation) -ForegroundColor Green -nonewline
-  Write-Host ($pwd) -ForegroundColor Green -nonewline
+  Write-Host ($currentDir) -ForegroundColor Green -nonewline
   Write-VcsStatus
   # Write-Host
   $LASTEXITCODE = $origLastExitCode
