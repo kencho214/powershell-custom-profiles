9e1287ea20767bb5fd3e30af5df44ed8dd85a928
ueda-kento-GLORY <ueda.kento@mail.glory.co.jp>
2024-09-06 14:41:00 +0900

    [feat] simplified prompt

--- custom-profile.ps1
+++ custom-profile.ps1
@@ -1,33 +1,23 @@
 # set dir name to load custom scripts automatically
 # PSCustomScripts must be defined as a environment variable
-Get-ChildItem $Env:PSCustomScripts\scripts | Where-Object Extension -eq ".ps1" | ForEach-Object { .$_.FullName }
 
-function prompt {
-  $origLastExitCode = $LASTEXITCODE
+#---------------------------------------------------------------------------
+# install posh-git, posh-sshell;
+#   PowerShellGet\Install-Module posh-sshell -Scope CurrentUser -Force
+#   PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+#---------------------------------------------------------------------------
 
-  $currentDir = $ExecutionContext.SessionState.Path.CurrentLocation.ToString()
+Import-Module posh-sshell
+Import-Module posh-git
 
-  if ($currentDir.contains($home)) {
-    $currentDir = $currentDir.Replace($home, "~")
-  }
-
-  $dirs = $currentDir -split "\\"
-  $currentDir = $dirs[0]
-  if ($dirs.Length -gt 2) {
-    $intermediates += $dirs[1..($dirs.Length - 2)] | ForEach-Object { $_.Substring(0, 1) }
-    $currentDir += "\"
-    $currentDir += $intermediates -join "\"
-  }
-  if ($dirs.Length -gt 1) {
-    $currentDir += "\" + $dirs[-1]
-  }
-
-  # Write-Host ($ExecutionContext.SessionState.Path.CurrentLocation) -ForegroundColor Green -nonewline
-  Write-Host ($currentDir) -ForegroundColor Green -nonewline
-  Write-VcsStatus
-  # Write-Host
-  $LASTEXITCODE = $origLastExitCode
-  "$(' $' * ($nestedPromptLevel + 1)) "
+if ($GitPromptSettings) {
+  $GitPromptSettings.DefaultPromptPrefix.Text = '[$(Get-Date -f "MM-dd HH:mm:ss")] '
+  $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
+  $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+  # $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+  $GitPromptSettings.DefaultPromptSuffix.Text = ' $ '
 }
+
+Get-ChildItem $Env:PSCustomScripts\scripts | Where-Object Extension -eq ".ps1" | ForEach-Object { .$_.FullName }
 
 Write-Host "Custom PowerShell Environment Loaded" -ForegroundColor DarkGray
